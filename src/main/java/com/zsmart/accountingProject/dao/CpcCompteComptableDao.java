22ba7b2db667b9bc2d9b355305d2049e0b30f6cb
hussein <aitdaoudlehoussein@gmail.com>
2020-04-28 16:20:23 +0000

    initial commit

---
+++ src/main/java/com/zsmart/accountingProject/dao/CpcCompteComptableDao.java
@@ -0,0 +1,26 @@
+package com.zsmart.accountingProject.dao;
+import com.zsmart.accountingProject.bean.CompteComptable;
+import com.zsmart.accountingProject.bean.CpcSousClasse;
+import com.zsmart.accountingProject.bean.CpcCompteComptable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+ public interface CpcCompteComptableDao extends JpaRepository<CpcCompteComptable,Long> {
+
+
+	 public CpcCompteComptable findByCompteComptable(CompteComptable compteComptable);
+	 public CpcCompteComptable findByCpcSousClasse(CpcSousClasse cpcSousClasse);
+
+	 public int deleteByCompteComptable(CompteComptable compteComptable);
+	 public int deleteByCpcSousClasse(CpcSousClasse cpcSousClasse);
+	 public int deleteByCpcSousClasseCpcId (Long id);
+	 @Query("SELECT NEW com.zsmart.accountingProject.bean.CpcCompteComptable(o.compteComptable,SUM(o.montant)) FROM OperationComptable o GROUP BY o.compteComptable")
+	 public List<CpcCompteComptable> find();
+
+}
